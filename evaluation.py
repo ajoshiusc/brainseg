2f291d3ecc1dbfb467770e6643c7bc879d0fef98
Anand Joshi <ajoshi@.usc.edu>
2022-04-18 17:09:58 -0700

    update

--- evaluation.py
+++ evaluation.py
@@ -2,28 +2,32 @@ import numpy as np
 from tqdm import tqdm
 from utils import test_images, test_single_nii, calculate_metric_percase
 from data_reader import H5DataLoader
-
+import h5py
 
 data_file = 'test_t1t2.h5'
 
 predictions_file = 'test_t1t2_output.h5'
+#predictions_file = 'test_t1.h5'
 
-test = H5DataLoader(data_file)
 
-pred = H5DataLoader(predictions_file)
+test = h5py.File(data_file, 'r')['Y']
+
+pred = h5py.File(predictions_file, 'r')['labels']
 
 num_classes = 9
-num_imgs = test.images.shape[0]
+num_imgs = 1000#test.shape[0]
 dice_coeffs = np.zeros([num_imgs,num_classes])
 hausdorff_dist = np.zeros([num_imgs,num_classes])
 
 
-for i in range(num_imgs):
+for i in tqdm(range(num_imgs)):
 
     metric_list = []
     for lid in range(num_classes):
-        dice_coeffs[i,lid], hausdorff_dist[i,lid] = calculate_metric_percase(test.labels[i] == lid, pred.labels[i] == lid)
+        dice_coeffs[i,lid], hausdorff_dist[i,lid] = calculate_metric_percase(test[i] == lid, pred[i] == lid)
 
+
+print(np.nanmean(dice_coeffs,axis=0)) #, np.nanstd(dice_coeffs,axis=0))
 
 
     
